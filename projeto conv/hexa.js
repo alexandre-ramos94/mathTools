ee9d599af00c0258fa47673985ae71ecfe49f641
José Alexandre Ramos Neto <65056403+alexandre-ramos94@users.noreply.github.com>
2020-05-27 17:58:04 -0300

    init

---
+++ projeto conv/hexa.js
@@ -0,0 +1,74 @@
+function decToHexa(){
+    var result = []
+    var fin = ''
+    var x = Number(document.getElementById('txtnum').value)
+    x= parseInt(x)
+    while(x>15){
+        result.unshift(x%16)
+        x=parseInt(x/16)
+      
+    }
+    result.unshift(x)
+    for(var c=0;c<result.length;c++)
+    switch(result[c]){
+        case 10: result[c] = 'A';
+        break;
+
+        case 11: result[c] = 'B';
+        break;
+
+        case 12: result[c] = 'C';
+        break;
+
+        case 13: result[c] = 'D';
+        break;
+
+        case 14: result[c] = 'E';
+        break;
+
+        case 15: result[c] = 'F';
+        break;
+        
+    }
+
+    for (c in result){
+        fin += result[c]
+    }
+    var g = document.getElementById('txtnum2')
+    g.value = fin
+}
+
+// função para fazer o inverso
+
+
+function HexToDec(){
+        var y= document.getElementById('txtnum2').value;
+        y = y.toUpperCase();
+        var resultado = 0
+        var aux = []
+        let contador = 0
+        var resp2 = document.getElementById('res2')
+
+        for(var c=y.length-1;c>=0;c--){
+            aux.push(y[contador])
+            if(aux[contador]=='A'){aux[contador]='10'}
+            else if(aux[contador]=='B'){aux[contador]='11'}
+            else if(aux[contador]=='C'){aux[contador]='12'}
+            else if(aux[contador]=='D'){aux[contador]='13'}
+            else if(aux[contador]=='E'){aux[contador]='14'}
+            else if(aux[contador]=='F'){aux[contador]='15'}
+            resultado += Number(aux[contador])*16**c
+            contador++
+        }
+        if(isNaN(resultado)){
+            resp2.innerHTML='Houve algum erro com os dados'
+        }
+        else{resp2.innerHTML=resultado}
+        var h = document.getElementById('txtnum')
+        h.value=resultado
+        
+}
+
+
+
+
