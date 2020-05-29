dc49af6be2f01566ad193c2f258a7494e555d2e8
José Alexandre Ramos Neto <65056403+alexandre-ramos94@users.noreply.github.com>
2020-05-28 22:49:49 -0300

    cpf tool

---
+++ scripts/cpf.js
@@ -0,0 +1,34 @@
+function verifica(){
+    let num=document.getElementById('txtnum').value
+    let aux = []
+    //Primeiro vamos colocar o numero recebido dentro de um array auxiliar, separado digito por digito
+    for (c in num){
+        aux.push(num[c])
+    }
+    //Agora vamos testar o primeiro digito verificador//
+    let x = num[9] //Esta é a posição do primeiro digito verificador
+    let sub = 0
+    let dec = 10
+    let result= 0
+    for (sub; sub<9; sub++){
+        result += dec*aux[sub]
+        dec--
+    }
+    result = result%11;
+    
+    //Agora vamos verificar o segundo digito verificador
+    let y = num[10] //Esta é a posição do segundo dígitor verificador
+    dec = 11
+    result2 = 0
+    for(let c=0; c<10; c++){
+        result2 += dec*aux[c]
+        dec--
+    }
+    result2 = result2%11;
+    //Agora vamos testar os resultados
+    if(x==11-result && y==11-result2){
+        let res = document.getElementById('res')
+        res.innerHTML+=' CPF VÁLIDO'
+    }
+    else{res.innerHTML='Resultado: '; res.innerHTML+= ' CPF Inválido'}
+}
