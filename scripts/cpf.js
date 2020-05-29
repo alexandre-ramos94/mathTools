0d322db2c9562055293893ba356861d327803ef9
José Alexandre Ramos Neto <65056403+alexandre-ramos94@users.noreply.github.com>
2020-05-29 19:01:05 -0300

    uodates

--- scripts/cpf.js
+++ scripts/cpf.js
@@ -3,10 +3,11 @@ function verifica(){
     let aux = []
     //Primeiro vamos colocar o numero recebido dentro de um array auxiliar, separado digito por digito
     for (c in num){
+        if (num[c]!= '.' && num[c] != '-'){
         aux.push(num[c])
-    }
+    }}
     //Agora vamos testar o primeiro digito verificador//
-    let x = num[9] //Esta é a posição do primeiro digito verificador
+    let x = aux[9] //Esta é a posição do primeiro digito verificador
     let sub = 0
     let dec = 10
     let result= 0
@@ -17,7 +18,7 @@ function verifica(){
     result = result%11;
     
     //Agora vamos verificar o segundo digito verificador
-    let y = num[10] //Esta é a posição do segundo dígitor verificador
+    let y = aux[10] //Esta é a posição do segundo dígitor verificador
     dec = 11
     result2 = 0
     for(let c=0; c<10; c++){
@@ -32,17 +33,17 @@ function verifica(){
         res.innerHTML+=' CPF VÁLIDO'
     }
     else{res.innerHTML='Resultado: '; res.innerHTML+= ' CPF Inválido'}
+
 }
 
 function limpa(){
     let num = document.getElementById('txtnum')
-    num.value = num.value.replace('-', '', '.', '')
-    if (num.value.length==11){
+    if (num.value.length==11 || num.value.length==14){
         verifica()       
     }
     else{
         let res = document.getElementById('res')
-        res.innerHTML='O cpf deve conter 11 Dígitos e somente numeros'
+        res.innerHTML='O cpf deve conter ao menos 11 dígitos. Apernas números ou no formato: 123.456.789-99'
     }
 }
 
